81e73c447b7ad27c56c998c4d18063b33de170fe
HyuntaePark <a01071712931@gmail.com>
2021-12-29 18:54:59 +0900

    Feat : 5th commit
    add feature importance

--- hyeontae_py/module.py
+++ hyeontae_py/module.py
@@ -562,7 +562,16 @@ def feature_engineering(train, test, encoder, scaler):
 
 
 def model(train, test, id, label, n_folds=5):
+    features = np.array(train)
     test_features = np.array(test)
+
+    # Extract feature names
+    feature_names = list(features.columns)
+
+    # Empty array for feature importances
+    feature_importance_values = np.zeros(len(feature_names))
+
+    # train, test id 탈락 시켜야함
     params = {"learning_rate": [0.1],
               "n_estimators": [200, 500],
               "max_depth": [1, 2, 8]}
@@ -591,6 +600,7 @@ def model(train, test, id, label, n_folds=5):
     lgb_best = lgb_model.best_estimator_
     lgb_score = round(lgb_model.best_score_, 6)
     lgb_parameter = lgb_model.best_params_
+    feature_importance_value = lgb_model.feature_importances_
 
     # predict y
     lgb_y_pred = lgb_best.predict(X_test)
@@ -654,105 +664,35 @@ def model(train, test, id, label, n_folds=5):
     submission = pd.DataFrame({'SK_ID_CURR': test[id], 'TARGET': test_prediction})
     submission.to_csv('prediction.csv', index=False)
 
+    feature_importances = pd.DataFrame({'feature': feature_names,
+                                        'importance': feature_importance_values})
 
-def oof_model(train, test, id, label, n_folds=5):
-    # Extract the ids
-    train_ids = train[id]
-    test_ids = test[id]
+    def plot_feature_importances(df):
+        # Sort features according to importance
+        df = df.sort_values('importance', ascending=False).reset_index()
 
-    # Extract the labels for training
-    labels = train[label]
+        # Normalize the feature importances to add up to one
+        df['importance_normalized'] = df['importance'] / df['importance'].sum()
 
-    # Remove the ids and target
-    train = train.drop(columns=[id, label])
-    test = test.drop(columns=[id])
+        # Make a horizontal bar chart of feature importances
+        plt.figure(figsize=(10, 6))
+        ax = plt.subplot()
 
-    # Extract feature names
-    feature_names = list(train.columns)
+        # Need to reverse the index to plot most important on top
+        ax.barh(list(reversed(list(df.index[:15]))),
+                df['importance_normalized'].head(15),
+                align='center', edgecolor='k')
 
-    # Convert to np arrays
-    features = np.array(train)
-    test_features = np.array(test)
+        # Set the yticks and labels
+        ax.set_yticks(list(reversed(list(df.index[:15]))))
+        ax.set_yticklabels(df['feature'].head(15))
 
-    # Create the kfold object
-    k_fold = KFold(n_splits=n_folds, shuffle=True, random_state=50)
+        # Plot labeling
+        plt.xlabel('Normalized Importance');
+        plt.title('Feature Importances')
+        plt.show()
 
-    # Empty array for feature importances
-    feature_importance_values = np.zeros(len(feature_names))
+        return df
 
-    # Empty array for test predictions
-    test_predict = np.zeros(test_features.shape[0])
-    test_predictions = np.zeros(test_features.shape[0])
+    plot_feature_importances(feature_importances)
 
-    # Empty array for out of fold validation predictions
-    out_of_fold2 = np.zeros(features.shape[0])
-    out_of_fold = np.zeros(features.shape[0])
-
-    # Lists for recording validation and training scores
-    valid_scores = []
-    train_scores = []
-
-    # Iterate through each fold
-    for train_indices, valid_indices in k_fold.split(features):
-        # Training data for the fold
-        train_features, train_labels = features[train_indices], labels[train_indices]
-        # Validation data for the fold
-        valid_features, valid_labels = features[valid_indices], labels[valid_indices]
-
-        # Create the model
-        model = lgb.LGBMClassifier(n_estimators=10000, objective='binary',
-                                   class_weight='balanced', learning_rate=0.05,
-                                   reg_alpha=0.1, reg_lambda=0.1,
-                                   subsample=0.8, n_jobs=-1, random_state=50)
-
-        # Train the model
-        model.fit(train_features, train_labels, eval_metric='auc',
-                  eval_set=[(valid_features, valid_labels), (train_features, train_labels)],
-                  eval_names=['valid', 'train'], categorical_feature='auto',
-                  early_stopping_rounds=100, verbose=200)
-
-        # Record the best iteration
-        best_iteration = model.best_iteration_
-
-        # Record the feature importances
-        feature_importance_values += model.feature_importances_ / k_fold.n_splits
-
-        # Make predictions
-        test_predict += model.predict(test_features, num_iteration=best_iteration) / k_fold.n_splits
-        test_predictions += model.predict_proba(test_features, num_iteration=best_iteration)[:, 1] / k_fold.n_splits
-
-        # Record the out of fold predictions
-        out_of_fold[valid_indices] = model.predict_proba(valid_features, num_iteration=best_iteration)[:, 1]
-        out_of_fold2[valid_indices] = model.predict(valid_features, num_iteration=best_iteration)
-
-        # Record the best score
-        valid_score = model.best_score_['valid']['auc']
-        train_score = model.best_score_['train']['auc']
-
-        valid_scores.append(valid_score)
-        train_scores.append(train_score)
-
-    # Make the submission dataframe
-    submission = pd.DataFrame({'SK_ID_CURR': test_ids, 'TARGET': test_predictions})
-
-    # Make the feature importance dataframe
-    feature_importances = pd.DataFrame({'feature': feature_names, 'importance': feature_importance_values})
-
-    # Overall validation score
-    valid_auc = roc_auc_score(labels, out_of_fold)
-    valid_f1 = f1_score(labels, out_of_fold2)
-
-    # Add the overall scores to the metrics
-    valid_scores.append(valid_auc)
-    train_scores.append(np.mean(train_scores))
-
-    # Needed for creating dataframe of validation scores
-    fold_names = list(range(n_folds))
-    fold_names.append('overall')
-
-    # Dataframe of validation scores
-    metrics = pd.DataFrame({'fold': fold_names,
-                            'train': train_scores,
-                            'valid': valid_scores})
-
-    return submission, feature_importances, metrics
