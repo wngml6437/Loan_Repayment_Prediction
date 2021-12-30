717b7f51403aee6b3fe0ebd708356c41e94bfd0d
Juhui Park <juhui6437@gmail.com>
2021-12-30 09:14:46 +0900

    feat: update flask web api

--- juhui_py/app.py
+++ juhui_py/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 import find
 app = Flask(__name__)
 
@@ -12,13 +12,18 @@ def search() :
 
 @app.route('/result/', methods = ["POST"])
 def result() :
-    customer_id = request.form['customer_id']
-    result = find.find_result(int(customer_id))
+    error = None
+    try:
+        customer_id = request.form['customer_id']
+        result = find.find_result(int(customer_id))
 
-    if result == 1 :
-        return render_template('result1.html')
-    elif result == 0 :
-        return render_template('result0.html')
+        if result == 1 :
+            return render_template('result1.html')
+        elif result == 0 :
+            return render_template('result0.html')
+    except:
+        error = 'Please Input Valid Value..!'
+        return render_template('search.html', error=error)
 
 
 if __name__ == '__main__' :
